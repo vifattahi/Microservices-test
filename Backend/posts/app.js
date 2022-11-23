216831ef101e371d76bd840ae9423fa0f88d16b8
mohammad.fattahi <vifattahi@gmail.com>
2022-11-23 10:34:52 +0330

    bug fix

--- Backend/posts/app.js
+++ Backend/posts/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const cors = require('cors');
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
